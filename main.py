376c77aa4e6ef3e287bd02fc9118387326c19b42
imvee <vind.veeru@gmail.com>
2023-08-21 01:21:31 +0530

    Streamlit inputs

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.set_page_config(
     page_icon="📔",
-    page_title="StoryGen"
+    page_title="StoryGen",
+    initial_sidebar_state="expanded"
 )
 
 hide_st_style ='''
@@ -15,3 +16,13 @@ header {visibility: hidden;}
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.title("📔StoryGen")
+
+with st.sidebar:
+    
+    idea=st.text_area("If you have any ideas, type in here...")
+    
+    story_type=st.selectbox("Story Type",["Bedtime Story","Moral Story","Fairytale","Adventure","Educational","Mystery",'Science Fiction'])
+    age=st.selectbox("Reader's Age",["2-4 years","5-7 years","8-10 years","10-12 years","12-14 years"])
+    length=st.select_slider("Story Length",["Short (~400 words)","Medium (~600 words)","Long (~800 words)"])
+    
+    start=st.button("Generate Story!")
